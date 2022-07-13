f27d1f9f3e3923033382d8746c9a37cbfe1df793
Mamadou Diallo <49839551+saliouprogress@users.noreply.github.com>
2022-07-13 15:05:13 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('btn');
 const itemName = document.getElementById('item-name');
 
-button.addEventListener('click', (_e) => {
+button.addEventListener('click', () => {
     itemName.innerText += '🔥';
 })
